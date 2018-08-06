a218bb6ffe88e4017945f9bc8cbf9fc92bb45d38
ueef <artem@arkadev.org>
2018-08-06 17:40:46 +0500

    initial

---
+++ src/AbstractException.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace Edn\Vip\Exceptions;
+
+use Throwable;
+
+abstract class AbstractException extends \Exception
+{
+    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    {
+        if (is_array($message)) {
+            foreach ($message as &$value) {
+                if (is_array($value) || is_object($value)) {
+                    $value = json_encode($value, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+                }
+            }
+
+            $message = sprintf(...$message);
+        }
+
+        parent::__construct($message, $code, $previous);
+    }
+}
