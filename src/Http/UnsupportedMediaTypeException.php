4d717917ef39080f0371348e1cb43a9849c9d91d
ueef <artem@arkadev.org>
2019-03-13 11:56:30 +0500

    UnsupportedMediaTypeException

---
+++ src/Http/UnsupportedMediaTypeException.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace Ueef\Exceptions\Http;
+
+use Throwable;
+use Ueef\Exceptions\AbstractException;
+use Ueef\Exceptions\Interfaces\HttpExceptionInterface;
+
+class UnsupportedMediaTypeException extends AbstractException implements HttpExceptionInterface
+{
+    public function __construct($message = "", ?Throwable $previous = null)
+    {
+        parent::__construct($message, 415, $previous);
+    }
+}
